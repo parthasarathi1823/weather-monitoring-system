5a45a5e71f88654455556dbe3db0c3f45299684a
parthasarathi1823 <dharmagaddaparthasarathi23@gmail.com>
2026-01-30 14:59:58 +0530

    feat: featching weather info ,API fetch

---
+++ fetch_weather.py
@@ -0,0 +1,14 @@
+import requests
+from config import API_KEY,API_URL
+
+def fetch_weather(city_name: str) -> dict :
+    payload={'key':API_KEY,'q':city_name}
+
+    response=requests.get(API_URL,params=payload)
+
+    response.raise_for_status()
+
+    json_data=response.json()
+
+
+    return json_data
